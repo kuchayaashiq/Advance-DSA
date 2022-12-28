a4465a3c73df1fcec15c89ad824549e9ad1456fd
kuchayaashiq <kucchayaashiq@gmail.com>
2022-12-28 19:37:32 +0530

    commets are added

--- src/Heaps/FurthestBuildingYouCanReach_Leetcode1642.java
+++ src/Heaps/FurthestBuildingYouCanReach_Leetcode1642.java
@@ -20,18 +20,29 @@ public class FurthestBuildingYouCanReach_Leetcode1642 {
     public static int furthestBuilding(int[] heights, int bricks, int ladders) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i = 1; i < heights.length; i++){
+            //difference between two heights of two consecative buildings
             int diff = heights[i] - heights[i-1];
+            // if this building is less or equal in heights
+            // then move forward without using bricks or ladders
             if(diff <= 0)
                 continue;
+            // if this building has greater height than previous building
+            // then use any one bricks or ladders to reach this height
             pq.add(diff);
+            //we put only three differences in priority Queue
             if(pq.size()>ladders){
+                // if size of pq is greater than ladders
+                // remove minimum difference and subtract that from bricks
+                // that means these bricks are used
                 int min = pq.remove();
                 bricks -= min;
             }
+            //if bricks are less than 0 that means all bricks are used
             if (bricks<0)
                 return i-1;
 
         }
+
         return heights.length-1;
     }
 }
